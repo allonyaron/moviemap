a6dbea939ec8d8a81a10b028f354091731171ac6
ayaron <allonyaron@gmail.com>
2015-02-24 16:36:00 +0000

    insert movie data to mongodb

--- import-movie.js
+++ import-movie.js
@@ -1,23 +1,25 @@
 var MongoClient = require('mongodb').MongoClient
   , request = require('request');
 
-MongoClient.connect('mongodb://localhost:27017/movie', function(err, db) {
+
+MongoClient.connect('mongodb://localhost:27017/mmfs', function(err, db) {
     if(err) throw err;
 
-    request('http://www.reddit.com/r/technology/.json', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var obj = JSON.parse(body);
 
-            var stories = obj.data.children.map(function (story) { return story.data; });
-/*
-            db.collection('reddit').insert(stories, function (err, data) {
+    request('http://data.tmsapi.com/v1.1/movies/showings?startDate=2015-02-24&zip=11215&radius=10&api_key=6ba9xurnyeebzp9dx4kr6ds7', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var movies = JSON.parse(body);
+
+            db.collection('movies').insert(movies, function (err, data) {
                     if(err) throw err;
 
                     console.dir(data);
 
                     db.close();
             });
-*/  
+ 
+ //"subType": "Feature Film"
+  
         }
     });
 });
